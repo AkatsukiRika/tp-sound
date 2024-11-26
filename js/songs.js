43374d6bf6c72080bce48585e8067ba8fcb834ca
AkatsukiRika <m1345860061@gmail.com>
2024-11-26 23:45:21 +0800

    feat: lyrics page

--- js/songs.js
+++ js/songs.js
@@ -2,27 +2,32 @@ const songs = [
   {
     id: 0,
     title: '迷星叫',
-    desc: '迷い星のうた'
+    desc: '迷い星のうた',
+    lyrics: './assets/raw/lyrics_content_0.html'
   },
   {
     id: 1,
     title: '影色舞',
-    desc: 'もうなにもかも 忘れて\n今宵は シルエットダンス'
+    desc: 'もうなにもかも 忘れて<br />今宵は シルエットダンス',
+    lyrics: './assets/raw/lyrics_content_1.html'
   },
   {
     id: 2,
     title: '壱雫空',
-    desc: '最初のひとしずくに 顔上げた今日の僕を'
+    desc: '最初のひとしずくに 顔上げた今日の僕を',
+    lyrics: './assets/raw/lyrics_content_2.html'
   },
   {
     id: 3,
     title: '栞',
-    desc: '“普通”とか“あたりまえ”ってなんだろう'
+    desc: '“普通”とか“あたりまえ”ってなんだろう',
+    lyrics: './assets/raw/lyrics_content_3.html'
   },
   {
     id: 4,
     title: '春日影',
-    desc: 'ねぇお願い どうかこのまま 離さないでいて'
+    desc: 'ねぇお願い どうかこのまま 離さないでいて',
+    lyrics: './assets/raw/lyrics_content_4.html'
   }
 ]
 
@@ -31,6 +36,10 @@ function generateTrackCards() {
     const card = getTrackCardInnerHTML(song)
     document.querySelector('.column').innerHTML += card
   })
+}
+
+function openLyricsPage(id) {
+  window.location.href = `./lyrics.html?id=${id}`
 }
 
 function getTrackCardInnerHTML(song) {
@@ -48,7 +57,7 @@ function getTrackCardInnerHTML(song) {
           <div class="divider">|</div>
           <div class="track-link">Instrumental</div>
           <div class="divider">|</div>
-          <div class="track-link">Lyrics</div>
+          <div class="track-link" onclick="openLyricsPage(${song.id})">Lyrics</div>
           <div class="divider">|</div>
         </div>
       </div>
