fc60d825eca18e2f1c3a9aa27789dd596c150f8e
AkatsukiRika <m1345860061@gmail.com>
2024-12-24 21:57:01 +0800

    feat: data list update

--- js/data/SongList.js
+++ js/data/SongList.js
@@ -9,47 +9,164 @@ export const TYPE_COVER = 3
 export const songList = [
   {
     id: 0,
-    title: '迷星叫',
-    desc: '迷い星のうた',
+    title: 'ズッ友 (TP Remix)',
+    desc: '愛がどーとか、いらん茶々です。',
     lyrics: './res/raw/lyrics_content_0.html',
     track: './res/raw/track_0.mp3',
-    cover: './res/drawable/img_cover_0.webp',
-    type: TYPE_ORIGINAL
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_REMIX
   },
   {
     id: 1,
-    title: '影色舞',
-    desc: 'もうなにもかも 忘れて<br />今宵は シルエットダンス',
+    title: 'Always Last Day',
+    desc: 'いつかまたあの夏の午後のように寄り添って',
     lyrics: './res/raw/lyrics_content_1.html',
     track: './res/raw/track_1.mp3',
-    cover: './res/drawable/img_cover_1.webp',
+    cover: './res/drawable/img_cover.webp',
     type: TYPE_ORIGINAL
   },
   {
     id: 2,
-    title: '壱雫空',
-    desc: '最初のひとしずくに 顔上げた今日の僕を',
+    title: '行军少年之歌',
+    desc: '两眼一睁，开始竞争！',
     lyrics: './res/raw/lyrics_content_2.html',
     track: './res/raw/track_2.mp3',
-    cover: './res/drawable/img_cover_2.webp',
+    cover: './res/drawable/img_cover.webp',
     type: TYPE_ORIGINAL
   },
   {
     id: 3,
-    title: '栞',
-    desc: '“普通”とか“あたりまえ”ってなんだろう',
+    title: '春日影 (IKU!!!!! Version)',
+    desc: 'ねぇお願い、どうかこのまま離さないでいて。',
     lyrics: './res/raw/lyrics_content_3.html',
     track: './res/raw/track_3.mp3',
-    cover: './res/drawable/img_cover_3.webp',
-    type: TYPE_ORIGINAL
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_COVER
   },
   {
     id: 4,
-    title: '春日影',
-    desc: 'ねぇお願い どうかこのまま 離さないでいて',
+    title: '死別 feat.私音ロウヤ',
+    desc: '僕らの最後は死別にしよう',
     lyrics: './res/raw/lyrics_content_4.html',
     track: './res/raw/track_4.mp3',
-    cover: './res/drawable/img_cover_4.webp',
+    cover: './res/drawable/img_cover.webp',
     type: TYPE_COVER
+  },
+  {
+    id: 5,
+    title: 'Lilied Isolationism',
+    desc: '幸せをきっと信じるのは<br />君がリリィだったから…',
+    lyrics: './res/raw/lyrics_content_5.html',
+    track: './res/raw/track_5.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_ORIGINAL
+  },
+  {
+    id: 6,
+    title: '涙橋 (TECHNO-POP RMX)',
+    desc: '就像站在烈日骄阳大桥上<br />眼泪狂奔滴落在我的脸庞',
+    lyrics: './res/raw/lyrics_content_6.html',
+    track: './res/raw/track_6.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_REMIX
+  },
+  {
+    id: 7,
+    title: '黄昏 ~comment te dire adieu~',
+    desc: 'どうぞ、あなた<br />さよならをください',
+    lyrics: './res/raw/lyrics_content_7.html',
+    track: './res/raw/track_7.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_COVER
+  },
+  {
+    id: 8,
+    title: 'No.10 (Anniversary Bootleg)',
+    desc: '10人目は君だよ！',
+    lyrics: './res/raw/lyrics_content_8.html',
+    track: './res/raw/track_8.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_REMIX
+  },
+  {
+    id: 9,
+    title: 'おどロボ (Waltz Arrange)',
+    desc: 'おどロボだけど、おどれるよ！',
+    lyrics: './res/raw/lyrics_content_9.html',
+    track: './res/raw/track_9.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_REMIX
+  },
+  {
+    id: 10,
+    title: 'TERMINAL',
+    desc: 'すごく長い旅、だからこそ…',
+    lyrics: './res/raw/lyrics_content_10.html',
+    track: './res/raw/track_10.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_ORIGINAL
+  },
+  {
+    id: 11,
+    title: '笑忘歌 (Japanese Version)',
+    desc: '楽しい人生を過ごせば<br />それだけで偉いよね',
+    lyrics: './res/raw/lyrics_content_11.html',
+    track: './res/raw/track_11.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_COVER
+  },
+  {
+    id: 12,
+    title: '明日が来ない',
+    desc: '一人の夜越えたなら<br />太陽がまた昇るかな？',
+    lyrics: './res/raw/lyrics_content_12.html',
+    track: './res/raw/track_12.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_ORIGINAL
+  },
+  {
+    id: 13,
+    title: 'Shinin\' Stage 2',
+    desc: 'I LOVE!<br />I\'m living all the love-life days.',
+    lyrics: './res/raw/lyrics_content_13.html',
+    track: './res/raw/track_13.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_ORIGINAL
+  },
+  {
+    id: 14,
+    title: '秋ゾラを嫉んでいる',
+    desc: 'アクセル止まらずに走って行こう<br />秋ゾラのハグを盗んだ',
+    lyrics: './res/raw/lyrics_content_14.html',
+    track: './res/raw/track_14.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_ORIGINAL
+  },
+  {
+    id: 15,
+    title: '非必要的歌',
+    desc: '自由的声音啊，墙挡不住',
+    lyrics: './res/raw/lyrics_content_15.html',
+    track: './res/raw/track_15.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_REMIX
+  },
+  {
+    id: 16,
+    title: 'Programmed Life 0x7E6',
+    desc: '或许终有一天，我们的生活将只剩下一日三餐与一块屏幕。',
+    lyrics: './res/raw/lyrics_content_16.html',
+    track: './res/raw/track_16.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_ORIGINAL
+  },
+  {
+    id: 17,
+    title: 'Rainbow Midnight',
+    desc: 'Place we live, calling "Hyper Nation".<br />We are on the run.',
+    lyrics: './res/raw/lyrics_content_17.html',
+    track: './res/raw/track_17.mp3',
+    cover: './res/drawable/img_cover.webp',
+    type: TYPE_ORIGINAL
   }
 ]
