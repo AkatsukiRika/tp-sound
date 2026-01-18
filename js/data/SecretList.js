d81ec5b8250975def9360197041555c07a7e4cb9
AkatsukiRika <m1345860061@gmail.com>
2026-01-18 20:19:54 +0800

    feat: secret content id=1001

--- js/data/SecretList.js
+++ js/data/SecretList.js
@@ -1,12 +1,12 @@
 import { ADAPTER_TYPE_SECRET_CARD } from "./SongList.js";
 
 export const secretList = [
-    {
-        adapterType: ADAPTER_TYPE_SECRET_CARD,
-        id: 1001,
-        title: 'Secret Test',
-        desc: 'Encrypted lyrics test entry',
-        lyrics: './res/raw/secret_test.json',
-        cover: './res/drawable/img_cover.webp'
-    }
+  {
+    adapterType: ADAPTER_TYPE_SECRET_CARD,
+    id: 1001,
+    title: '大正ロマン',
+    desc: '大正スタイルのアイドルソング',
+    lyrics: './res/raw/secret_0.json',
+    cover: './res/drawable/img_cover.webp'
+  }
 ]
